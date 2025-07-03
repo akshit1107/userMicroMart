78ee6ac5c218550be2675aa68ef0c8a19c6b13db
Akshit Tyagi <this.akshit.11@gmail.com>
2025-07-03 06:58:15 +0530

    project setup

---
+++ src/main/java/dev/akshit/usermicromart/UserMicroMartApplication.java
@@ -0,0 +1,13 @@
+package dev.akshit.usermicromart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserMicroMartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserMicroMartApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/akshit/usermicromart/UserMicroMartApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.akshit.usermicromart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserMicroMartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
